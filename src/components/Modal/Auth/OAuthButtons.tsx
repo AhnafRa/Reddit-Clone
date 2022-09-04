32d8d17ed6a8a526bf84468d0c0bd64f51ecc0e0
AhnafRa <92684690+AhnafRa@users.noreply.github.com>
2022-09-04 19:09:31 -0400

    Debug

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebase/clientApp";
@@ -16,7 +16,7 @@ const OAuthButtons: React.FC = () => {
         <Image src="/images/googlelogo.png" height="20px" mr={4} />
         Continue with Google
       </Button>
-      {error && <text>{error}</text>}
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
